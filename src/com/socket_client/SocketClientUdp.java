de261ca1fbf0e2f7d47805d852303185dfba077a
dmg01137 <130947976+dmg01137@users.noreply.github.com>
2024-07-10 11:16:04 +0900

    0710_2

--- src/com/socket_client/SocketClientUdp.java
+++ src/com/socket_client/SocketClientUdp.java
@@ -18,16 +18,26 @@ public class SocketClientUdp {
             clientSocket = new DatagramSocket();
             
             // 전송할 메시지 준비
-            String message = "Hello,I'm jiwon";
+            String message = "Hello, I'm jiwon";
             byte[] sendData = message.getBytes();
             
-            // UDP 패킷 생성
+            // UDP 패킷 생성 및 서버로 전송
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
-            
-            // 서버로 패킷 전송
             clientSocket.send(sendPacket);
-            
             System.out.println("보낸 메세지: " + message);
+            
+            // 서버로부터의 응답을 받기 위한 패킷 준비
+            byte[] receiveBuffer = new byte[1024];
+            DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+            
+            // 서버에서 패킷 수신
+            clientSocket.receive(receivePacket);
+            
+            // 수신된 데이터 추출 및 출력
+            byte[] receivedData = receivePacket.getData();
+            int length = receivePacket.getLength();
+            String serverMessage = new String(receivedData, 0, length);
+            System.out.println("서버: " + serverMessage);
             
         } catch (Exception e) {
             e.printStackTrace();
