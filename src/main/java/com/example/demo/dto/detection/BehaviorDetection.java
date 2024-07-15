957a56056576cd65c2c4bfdfb17eb4842df9a41e
dmg01137 <130947976+dmg01137@users.noreply.github.com>
2024-07-15 11:58:13 +0900

    user,log,policy add.delete

--- src/main/java/com/example/demo/dto/detection/BehaviorDetection.java
+++ src/main/java/com/example/demo/dto/detection/BehaviorDetection.java
@@ -4,24 +4,52 @@ import java.time.LocalDateTime;
 
 public class BehaviorDetection {
 
-    private Integer detection_number;
+    private Integer detection_number;  // 탐지 번호
     private String s_to_ip;           // 소스와 목적지 IP 정보
-    private String s_ip;
-    private String d_ip;
-    private Integer s_port;
-    private Integer d_port;
-    private LocalDateTime create_dt;
-    private LocalDateTime modify_dt;
-    private String action_type;
-    private String policy_name;
-    private String policy_info;
-    private String pattern_1;
-    private String pattern_2;
-    private String pattern_3;
-    private Integer dangerous;
-    private Integer base_cnt;
-    private Integer comp_cnt;
-    private Integer base_time;
+    private String s_ip;              // 소스 IP
+    private String d_ip;              // 목적지 IP
+    private Integer s_port;           // 소스 포트
+    private Integer d_port;           // 목적지 포트
+    private LocalDateTime create_dt;  // 생성 일시
+    private LocalDateTime modify_dt;  // 수정 일시
+    private String action_type;       // 동작 유형
+    private String policy_name;       // 정책 이름
+    private String policy_info;       // 정책 정보
+    private String pattern_1;         // 패턴 1
+    private String pattern_2;         // 패턴 2
+    private String pattern_3;         // 패턴 3
+    private Integer dangerous;        // 위험도
+    private Integer base_cnt;         // base_cnt
+    private Integer comp_cnt;         // comp_cnt
+    private Integer base_time;        // base_time
+
+    // 생성자
+    public BehaviorDetection() {
+    }
+
+    public BehaviorDetection(Integer detection_number, String s_to_ip, String s_ip, String d_ip, Integer s_port, Integer d_port,
+                             LocalDateTime create_dt, LocalDateTime modify_dt, String action_type, String policy_name,
+                             String policy_info, String pattern_1, String pattern_2, String pattern_3, Integer dangerous,
+                             Integer base_cnt, Integer comp_cnt, Integer base_time) {
+        this.detection_number = detection_number;
+        this.s_to_ip = s_to_ip;
+        this.s_ip = s_ip;
+        this.d_ip = d_ip;
+        this.s_port = s_port;
+        this.d_port = d_port;
+        this.create_dt = create_dt;
+        this.modify_dt = modify_dt;
+        this.action_type = action_type;
+        this.policy_name = policy_name;
+        this.policy_info = policy_info;
+        this.pattern_1 = pattern_1;
+        this.pattern_2 = pattern_2;
+        this.pattern_3 = pattern_3;
+        this.dangerous = dangerous;
+        this.base_cnt = base_cnt;
+        this.comp_cnt = comp_cnt;
+        this.base_time = base_time;
+    }
 
     // Getter 및 Setter 메서드
 
